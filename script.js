f0dbfb208f594892f28953ebf8b9cde04fd6872b
Rumeysa Koseoglu <koseoglurumeysa0@gmail.com>
2025-03-05 19:27:56 +0300

    feat: add initial game setup with element references and winning patterns

---
+++ script.js
@@ -0,0 +1,17 @@
+let btnRef = document.querySelectorAll(".button-option");
+let popupRef = document.querySelector(".popup");
+let newGameBtn = document.getElementById("new-game");
+let restartBtn = document.getElementById("restart");
+let msgRef = document.getElementById("message");
+
+//winning pattern array
+let winingPattern = [
+    [0,1,2],
+    [0,3,6],
+    [2,5,8],
+    [6,7,8],
+    [3,4,5],
+    [1,4,7],
+    [0,4,8],
+    [2,4,6],
+];
